73d3d625d06e8b8f39d26d22bd41951c372ecc79
Zack Ottesen <zo@pyra.dk>
2024-02-15 09:34:30 +0100

    update server

--- 02a._Server/fileConverterServer/fileConverterServerCSharp/Program.cs
+++ 02a._Server/fileConverterServer/fileConverterServerCSharp/Program.cs
@@ -18,11 +18,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/json", () =>
     {
         FileConverter fc = new FileConverter();
@@ -62,25 +57,3 @@ app.MapGet("/yaml", () =>
     })
     .WithName("jamlFile")
     .WithOpenApi();
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
